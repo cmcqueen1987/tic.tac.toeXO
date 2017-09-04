571979792ed518b04a0b976528e35c20ec407aae
cmcqueen1987 <charlie.mcqueen87@gmail.com>
2017-09-03 20:10:10 -0400

    @

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -192,10 +192,10 @@ module.exports = {
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
-// const authEvents = require('./auth/events.js')
+const authEvents = require('./auth/events.js')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
-// $(() => {
-// authEvents.addHandlers()
-// })
+$(() => {
+  authEvents.addHandlers()
+})
